17eaaa56dd0ee6d9d46e5f4a1cec30d722be0f8c
junusi <82803045+junusi@users.noreply.github.com>
2021-05-17 22:23:15 +0300

    Rendering socials#12

--- js/home.js
+++ js/home.js
@@ -1,7 +1,10 @@
+   
 import { renderClock } from './components/clock/clock.js';
+import { socials } from './components/socials/socials.js';
+import { socialsData } from './data/socialsData.js';
 
-renderClock('#hero_clock', '04-19 10:00:00');     
-
+renderClock('#hero_clock', '04-19 10:00:00');
+socials('#socials_block', socialsData);
 
 
 
