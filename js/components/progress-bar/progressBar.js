b4579575af97c0e02b9f9580597310b50561566c
junusi <82803045+junusi@users.noreply.github.com>
2021-05-18 09:50:13 +0300

    Progress-bar

---
+++ js/components/progress-bar/progressBar.js
@@ -0,0 +1,24 @@
+function progressBar(selector, data) {
+    // validation
+
+    // logic
+    const DOM = document.querySelector(selector);
+    let HTML = '';
+
+    for (const progress of data) {
+        HTML += `<div class="progress-bar">
+                    <div class="top">
+                        <div class="label">${progress.label}</div>
+                        <div class="value">${progress.value}</div>
+                    </div>
+                    <div class="bottom">
+                        <div class="progress"></div>
+                    </div>
+                </div>`;
+    }
+
+    // result return
+    DOM.insertAdjacentHTML('afterend', HTML);
+}
+
+export { progressBar }
